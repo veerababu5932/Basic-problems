ea524bb5e635c3166163758e5c6156ea418f9aa1
veerababu5932 <babuveera8579@gmail.com>
2024-10-05 09:33:07 +0530

    Create second number.c

---
+++ second number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int average;
+    int first;
+    scanf("%d",&average);
+    scanf("%d",&first);
+    int second=((average*2)-first);
+    printf("%d",second);
+}
