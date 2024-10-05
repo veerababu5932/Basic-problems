d048f4fc4b26b694a082ce4f63f6b6819c8f8312
veerababu5932 <babuveera8579@gmail.com>
2024-10-05 10:47:15 +0530

    Create surface volume of cube.c

---
+++ surface volume of cube.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int b = (6*a*a);
+    int c = (a*a*a);
+    printf("Surface area = %d and Volume = %d",b,c);
+    return 0;
+}
